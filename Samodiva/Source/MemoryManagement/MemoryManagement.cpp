56668b4c5d3fa5afd2849b3b465cc81974714fd3
Nikola Dimitroff <dimitroff.nikola@gmail.com>
2016-10-29 22:28:42 +0300

    Added doctest. Added a Test debug config to run tests.

--- Samodiva/Source/MemoryManagement/MemoryManagement.cpp
+++ Samodiva/Source/MemoryManagement/MemoryManagement.cpp
@@ -3,7 +3,28 @@
 
 namespace Samodiva
 {
+#ifdef SAMODIVA_TEST_BUILD
+class MallocAllocator : public Samodiva::IAllocator
+{
+public:
+	virtual void* Malloc(size_t size, unsigned)
+	{
+		return std::malloc(size);
+	}
+	virtual void Free(void* ptr)
+	{
+		std::free(ptr);
+	}
+	virtual void* Realloc(void* ptr, size_t newSize)
+	{
+		return std::realloc(ptr, newSize);
+	}
+};
+MallocAllocator g_TestingMallocAllocator;
+Samodiva::IAllocator* g_Allocator = &g_TestingMallocAllocator;
+#else
 Samodiva::IAllocator* g_Allocator = nullptr;
+#endif
 thread_local Samodiva::LinearAllocator<tls_TempAllocatorSize> Samodiva::TempAllocator::tls_Alloc;
 }
 
