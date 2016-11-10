81563f35f74cf2a2f3659868d6710d37faaf1c6c
Nikola Dimitroff <dimitroff.nikola@gmail.com>
2016-11-10 09:00:52 +0200

    Added a forgotten file.

---
+++ Samodiva/Source/Tests/TestMain.cpp
@@ -0,0 +1,35 @@
+#define DOCTEST_CONFIG_IMPLEMENT
+#define DOCTEST_CONFIG_NO_EXCEPTIONS
+#include <doctest/doctest.h>
+#include <iostream>
+
+int main(int argc, char** argv) {
+	doctest::Context context; // initialize
+
+	// defaults
+	context.setOption("sort", "name"); // sort the test cases by their name
+
+	context.applyCommandLine(argc, argv);
+
+	// overrides
+	context.setOption("no-breaks", true);             // don't break in the debugger when assertions fail
+
+	int res = context.run(); // run
+
+	if (context.shouldExit()) // important - query flags (and --exit) rely on the user doing this
+		return res;          // propagate the result of the tests
+
+	std::cout << "Press any key to exit" << std::endl;
+	char x;
+	std::cin >> x;
+	
+}
+
+TEST_SUITE("Suite for doctest");
+
+SCENARIO("making sure tests work")
+{
+	CHECK(true);
+}
+
+TEST_SUITE_END();
